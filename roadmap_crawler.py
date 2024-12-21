90a67de0fa1b399869cab5961cc9bd4d7c85757d
Nightingale <93238522+CITATS928@users.noreply.github.com>
2024-12-20 21:27:56 -0500

    sort based on parent title

--- roadmap_crawler.py
+++ roadmap_crawler.py
@@ -47,12 +47,23 @@ def get_roadmap_with_selenium():
         title = driver.find_element(By.TAG_NAME, 'h1').text.strip()
 
         # find all the elements that include 'data-title', then get the value of 'data-title'
-        element = driver.find_elements(By.XPATH, '//*[@data-title]')
-        data_titles = [item.get_attribute('data-title') for item in element]
+        element = driver.find_elements(By.XPATH, '//*[@data-title and @data-parent-title]')
+        # data_titles = [item.get_attribute('data-title') for item in element]
+
+        roadmap_data = {}
+        for item in element:
+            parent_title = item.get_attribute('data-parent-title')
+            data_title = item.get_attribute('data-title')
+
+            # sort the data based on parent_title
+            if parent_title not in roadmap_data:
+                roadmap_data[parent_title] = []
+            roadmap_data[parent_title].append(data_title)
+
 
         return {
             'title': title,
-            'steps': data_titles
+            'steps': roadmap_data
         }
     finally:
         driver.quit()
