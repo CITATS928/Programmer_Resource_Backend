3f806f21096977ffaba686db56c045be3f1e5e71
Nightingale <93238522+CITATS928@users.noreply.github.com>
2024-12-28 23:10:37 -0500

    project scrape basic

---
+++ project_scrape.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import json
+
+
+def scrape_project():
+    options = Options()
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--start-maximized")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    
+
+    try:
+
+        projects = []
+        max_projects = 10
+        url = ""
+
+        driver.get(url)
+        time.sleep(5)
+
+        while len(projects) < projects:
+            pass
+
+        
+    finally:
+        driver.quit()
+
+
+
+
+def save_courses(data, filename='frontend_projects.json'):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+    print(f"Courses saved to {filename}")
+    
+
+if __name__ == '__main__':
+    projects = scrape_project()
+    if projects:
+        save_courses(projects)
