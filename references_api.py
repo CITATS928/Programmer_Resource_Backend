f359c7b3d0532b9070e08431b2372c8c0f5c866b
Nightingale <93238522+CITATS928@users.noreply.github.com>
2025-01-02 21:43:42 -0500

    references merge 2/3, added options to compatible with Docker

--- references_api.py
+++ references_api.py
@@ -1,5 +1,13 @@
 import requests
 import json
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def fetch_udemy_courses():
     """
@@ -46,14 +54,81 @@ def fetch_udemy_courses():
     
 
 def scrape_books():
-    pass
+    """
+    
+    """
+    options = Options()
+    # Add headless mode and other options to compatible with Docker
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--start-maximized")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    try:
+        books = []
+        max_books = 10
+        url = "https://www.goodreads.com/search?page=1&q=frontend&qid=ZLdchY1S03&search_type=books&tab=books&utf8=%E2%9C%93"
+
+        driver.get(url)
+        time.sleep(5)
+
+        while len(books) < max_books:
+            # wait for the page to load
+            try:
+                WebDriverWait(driver, 30).until(
+                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".tableList .bookTitle"))
+                )
+            except Exception as e:
+                print("Error: TimeoutException occurred.")
+                raise e
+            
+            book_elements = driver.find_elements(By.CSS_SELECTOR, ".tableList .bookTitle")
+            author_elements = driver.find_elements(By.CSS_SELECTOR, ".tableList .authorName")
+
+            for book, author in zip(book_elements, author_elements):
+                try:
+                    title = book.text.strip()
+                    link = book.get_attribute("href")
+                    author = author.text.strip()
+                    books.append({
+                        "title": title,
+                        "author": author,
+                        "link": link
+                    })
+                except Exception as e:
+                    print(f"Error extracting book title: {e}")
+
+            # check reach max
+            if len(books) >= max_books:
+                break
+
+            # next page
+            try:
+                next_page = driver.find_element(By.CSS_SELECTOR, ".next_page")
+                if "disabled" in next_page.get_attribute("class"):
+                    break
+                next_page.click()
+                time.sleep(5)
+            except Exception as e:
+                print("Error: No more pages.")
+                break
+        return books
+                    
+    finally:
+        driver.quit()
 
 
 def scrape_projects():
     pass
 
 
-def save_to_file(data, filename="frontend_courses.json"):
+def save_to_file(data, filename="frontend_references.json"):
     """
     
     """
